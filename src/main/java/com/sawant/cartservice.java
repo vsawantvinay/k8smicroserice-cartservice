7dcdd8c12434d554c295a1caf7305edc1700bf07
vsawantvinay <vsawant.azure@gmail.com>
2026-07-17 01:27:04 +0530

    Refactor Cartservice class and update annotations
    Refactor Cartservice class to follow Java naming conventions and fix annotations.

--- src/main/java/com/sawant/cartservice.java
+++ src/main/java/com/sawant/cartservice.java
@@ -1,6 +1,11 @@
 package com.sawant;
-@restcontroller
-public class cartservice {
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Cartservice {
+    
     @GetMapping ("/cartservice")
     public String cartservic() {
         return "Welcome to my cart service";
